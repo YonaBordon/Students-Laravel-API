1dfe191bc7657a09b1cd17b8a33b98c08bc486bd
Shonakar <carlosjbordon@gmail.com>
2022-03-18 18:35:35 -0300

    add validations

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
+use Dotenv\Validator;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 
 //Creado con make:controller
@@ -31,17 +33,32 @@ class StudentController extends Controller
 
     public function addStudent(Request $request)
     {
-        $student = new Student;
-        $student->name = $request->input('name');
-        $student->course = $request->input('course');
-        $student->email = $request->input('email');
-        $student->phone = $request->input('phone');
-        $student->save();
 
-        return response()->json([
-            'status' => 200,
-            'message' => 'Student Added Successfully',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'course' => 'required|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|numeric|max:10|min:10',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validate_err' => $validator->messages(),
+            ]);
+        } else {
+
+            $student = new Student;
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+            $student->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Added Successfully',
+            ]);
+        }
     }
 
     public function updateStudent(Request $request, $id)
