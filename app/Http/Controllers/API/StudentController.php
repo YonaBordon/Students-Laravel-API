26fa5b2253fb3281074113d7bf82bb8f8b478aa6
Shonakar <carlosjbordon@gmail.com>
2022-03-20 02:45:37 -0300

    data validations

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
-use Dotenv\Validator;
-use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 //Creado con make:controller
 
@@ -25,25 +24,34 @@ class StudentController extends Controller
     public function getStudent($id)
     {
         $student = Student::find($id);
-        return response()->json([
-            'status' => 200,
-            'student' => $student,
-        ]);
+        if ($student) {
+            return response()->json([
+                'status' => 200,
+                'student' => $student,
+            ]);
+        } else {
+
+            return response()->json([
+                'status' => 404,
+                'message' => 'No Student ID Found',
+            ]);
+        }
     }
 
     public function addStudent(Request $request)
     {
 
+
         $validator = Validator::make($request->all(), [
             'name' => 'required|max:191',
             'course' => 'required|max:191',
             'email' => 'required|email|max:191',
-            'phone' => 'required|numeric|max:10|min:10',
+            'phone' => 'required|max:10|min:10',
         ]);
 
         if ($validator->fails()) {
             return response()->json([
-                'validate_err' => $validator->messages(),
+                'errors' => $validator->errors(),
             ]);
         } else {
 
@@ -63,17 +71,41 @@ class StudentController extends Controller
 
     public function updateStudent(Request $request, $id)
     {
-        $student = Student::find($id);
-        $student->name = $request->input('name');
-        $student->course = $request->input('course');
-        $student->email = $request->input('email');
-        $student->phone = $request->input('phone');
-        $student->update();
-
-        return response()->json([
-            'status' => 200,
-            'message' => 'Student Updated Successfully',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'course' => 'required|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|max:10|min:10',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ]);
+            
+        } else {
+            $student = Student::find($id);
+
+            if ($student) {
+
+
+                $student->name = $request->input('name');
+                $student->course = $request->input('course');
+                $student->email = $request->input('email');
+                $student->phone = $request->input('phone');
+                $student->update();
+
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Student Updated Successfully',
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'No Student ID Found',
+                ]);
+            }
+        }
     }
 
     public function deleteStudent($id)
